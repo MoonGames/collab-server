24d610f3df0676aaec46cd7ce0961e0b401630fc
Martin Indra <aktive@seznam.cz>
2012-10-22 08:53:58 +0200

    Removing unused comments.

--- src/cz/mgn/collabserver/http/HTTPServer.java
+++ src/cz/mgn/collabserver/http/HTTPServer.java
@@ -18,10 +18,6 @@
  * along with Collab desktop.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.mgn.collabserver.http;
 
 import java.awt.image.BufferedImage;
